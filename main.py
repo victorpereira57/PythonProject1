d454b003fff9c88812a0147482d070007783427b
victorpereira57 <163787042+victorpereira57@users.noreply.github.com>
2026-08-14 19:31:31 -0300

    Criação do Programa de idade

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Seja bem vindo")
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+if idade <18:
+    print("Voce é menor de idade")
+else:
+    print("Vocé é maior de idade")
+
+
+
